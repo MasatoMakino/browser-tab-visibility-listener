8d558a5a5910f7bf925d779ece5dffa49985044a
MasatoMakino <unagiinu.g@gmail.com>
2021-04-11 16:52:46 +0900

    update : demo page #2

--- demoSrc/demo.js
+++ demoSrc/demo.js
@@ -7,9 +7,11 @@ window.onload = () => {
   const listener = new BrowserTabVisibilityListener();
   listener.on(BrowserTabVisibilityEvent.VISIBLE, () => {
     console.log("visible");
+    window.visibleState = "visible"; //for selenium test
   });
   listener.on(BrowserTabVisibilityEvent.INVISIBLE, () => {
     console.log("invisible");
+    window.visibleState = "invisible"; //for selenium test
   });
 
   const message = document.createElement("span");
