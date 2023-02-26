6bf703bac0741198b1bbbd87d1efac967d930028
MasatoMakino <makino.masato.g@gmail.com>
2023-02-26 13:34:46 +0900

    add : gh-pages.yml

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  collectCoverageFrom: ["src/**/*.ts"],
-  testEnvironment: "node",
-  setupFilesAfterEnv: ["<rootDir>/__test__/global.ts"],
-};
